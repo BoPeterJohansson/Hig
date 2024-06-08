bf79ff4e22ec0463248637b3f06a11b5119b0d49
Peter Johansson <bo.peter.johansson@gmail.com>
2024-06-09 04:39:01 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,4 @@ import streamlit as st
 print('Hej, jag heter Peter')                          
 print('Jag vill lära mig programmering för att jag studerar Business Intelligence och jag vill jobba med data och datanalys')
 
-streamlit run
+st.streamlit run
